de7cfcdcb32e37d8873114392e59f71c0100029c
Dragan Savevski <dragansavevski@outlook.com>
2018-01-31 19:09:25 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VCalendar from 'v-calendar';
+
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  VDivider,
+  VSubheader,
+  VTooltip,
+  transitions
+} from 'vuetify'
+
+import '../node_modules/vuetify/src/stylus/app.styl'
+import 'v-calendar/lib/v-calendar.min.css';
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    VDivider,
+    VSubheader,
+    VTooltip,
+    transitions
+  }
+})
+
+Vue.use(VCalendar, {
+  firstDayOfWeek: 2, // Set first day of week to Sunday
+  popoverExpanded: true,
+  popoverVisibility: 'visible',
+  datePickerInputClass: 'datepicker-input',
+  datePickerInputPlaceholder: 'FSADASDDA'
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
